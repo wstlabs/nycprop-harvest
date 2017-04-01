d5784af05de8a04956639ec315d2a40be656e6ad
wstlabs <notyet@notyet.org>
2017-04-01 13:42:32 -0400

    enhanced logging for exceptional parse cases

--- crawl/parse.py
+++ crawl/parse.py
@@ -4,6 +4,17 @@ from collections import OrderedDict
 from crawl.pdfutil import textify
 from crawl.logging import log
 
+pat = {}
+pat['integer'] = re.compile('^\d+$')
+pat['abatement'] = re.compile('^.*abatement',re.IGNORECASE)
+pat['statement-billing-summary'] = re.compile('^.*Statement Billing Summary',re.IGNORECASE)
+def matches_pattern(page,name):
+    regex = pat[name]
+    for line in page:
+        if re.match(regex,line):
+            return True
+    return False
+
 
 
 def extract_between(page,startlabel,endlabel,offset=1):
@@ -66,54 +77,62 @@ def extract_amount_after(page,label):
         elif found and line.startswith("$"):
             return line
 
-def extract_balance(bigpage):
+def extract_total_amount_due(bigpage):
     balance = extract_after(bigpage,'Total amount due by',-1,2)
     if balance is not None and balance.startswith("$"):
         return balance
     return extract_amount_after(bigpage,'Total amount due by')
 
+def extract_generic_amount_due(page):
+    return extract_after(page,'Amount Due',0)
+
 def parse_general(pages,bigpage):
     x = OrderedDict()
+    x['htype'] = 1 if matches_pattern(pages[0],'statement-billing-summary') else 2
     x['tax-class'] = extract_after(bigpage,'Tax class',0)
     x['owner-name'] = extract_between(pages[0],'Owner name:','Property address:')
     x['mailing-address'] = extract_mailing_address(pages[0])
     market_value = extract_after(bigpage,'Estimated market value',0)
-    balance = extract_balance(bigpage)
     x['estimated-market-value'] = scrub_amount(market_value)
+    if x['htype'] == 1:
+        balance = extract_total_amount_due(bigpage)
+    else:
+        balance = extract_generic_amount_due(pages[0])
     x['total-amount-due'] = scrub_amount(balance)
     return x
 
+def extract_unitcount(page):
+    """Extracts what appears to be the stabilized unit count from a given page.
+    Our current hypothesis is that whenever string appears directly after the
+    label 'Housing-Rent Stabilization', -and- this string looks like an integer,
+    then that's the unit count (and this number will be the same regardless of
+    how many times such a match occurs throughout the page).
+    If more than one match is detected, we simply emit the first matching value
+    and make a note of this weirdness in the logs."""
+    rawvals = list(yield_after(page,'Housing-Rent Stabilization'))
+    log.info("RAW %s" % rawvals)
+    intlike = (int(_) for _ in rawvals if re.match(pat['integer'],_))
+    counts  = sorted(set(intlike))
+    if len(counts) < 1:
+        if rawvals:
+            log.info("WEIRD rawvals = %s but none are integer" % rawvals)
+        return None
+    if len(counts) > 1:
+        log.info("WEIRD too many unitcount values %s" % counts)
+    return counts[0]
+
 def parse_stabilization(pages,bigpage):
     x = OrderedDict()
-    x['count'] = extract_after(bigpage,'Housing-Rent Stabilization',-1)
+    x['unitcount'] = extract_unitcount(bigpage)
     return x
 
 def parse(f):
     pages = list(textify(f))
     bigpage = reduce(lambda x, y: x+y,pages)
     d = OrderedDict()
-    d['general'] = parse_general(pages,bigpage)
+    d['general']       = parse_general(pages,bigpage)
     d['stabilization'] = parse_stabilization(pages,bigpage)
     return d
 
 
-
-
-#
-# deprecated stuff
-#
-
-pat = {}
-pat['abatement'] = re.compile('^.*abatement',re.IGNORECASE)
-def _has_abatement(block):
-    for line in block:
-        if re.match(pat['abatement'],line):
-            return True
-    return False
-
-def _extract_abatement(block):
-    for line in block:
-        if re.match(pat['abatement'],line):
-            return line
-    return None
 
