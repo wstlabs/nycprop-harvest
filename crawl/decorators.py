ee05902fb11d1f128204f99cef41fbbb294e8d6f
wstlabs <wst@pobox.com>
2017-04-01 20:02:29 -0400

    better retry loop

--- crawl/decorators.py
+++ crawl/decorators.py
@@ -20,3 +20,21 @@ def timedmulti(func):
         return tuple(list(t) + [delta])
     return called
 
+
+def backoff(func=None,retry=1,interval=5,log=None):
+    @wraps(func)
+    def wrapped(*args,**kwargs):
+         i = 0 
+         while i < retry:
+             if log:
+                 log.info("retry = %d .." % i)
+             try:
+                 return func(*args,**kwargs)
+             except Exception as e:
+                 if log:
+                     log.info("WHOA count=%d, reason: = %s" % (i,e))
+                     log.exception(e) 
+                 i += 1
+                 time.sleep(interval)
+         raise RuntimeError("retry limit exceeded")
+    return wrapped 
